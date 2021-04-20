3b47f9b5f7eef4d20487f8e085816fa3faa5a278
Franco Benitez <franco_casla_benitez@hotmail.com>
2021-04-20 16:36:01 -0300

    update

---
+++ src/models/Servicio.js
@@ -0,0 +1,53 @@
+const db = require('../database/database');
+
+class ServicioModel{
+    getAll(){
+        return new Promise((resolve,reject)=>{
+            db.query("SELECT * FROM servicios",(err,res,fields)=>{
+                if(err) throw err;
+                resolve(res);
+            })
+        })
+    }
+
+    findById(id){
+        return new Promise((resolve,reject)=>{
+            db.query("SELECT * FROM servicios WHERE id = ?",[id],(err,res,fields)=>{
+                if(err) throw err;
+                resolve(res);
+            })
+        })
+    }
+    
+    create(body){
+        return new Promise((resolve,reject)=>{
+            db.query("INSERT INTO servicios(idCasa, luz, agua, calefaccion, telefono, gas, internet) VALUES (?, ?, ?, ?, ?, ?, ?)",
+            [body.idCasa, body.luz, body.agua, body.calefaccion, body.telefono, body.gas, body.internet],(err,res,fields)=>{
+                if(err) throw err;
+                resolve(res);
+            })
+        })
+    }
+
+    update(body){
+        return new Promise((resolve,reject)=>{
+            db.query("UPDATE servicios SET  luz =?, agua =?, calefaccion =?, telefono=?, gas=?, internet=? WHERE idCasa = ?",
+            [body.luz, body.agua, body.calefaccion, body.telefono, body.gas, body.internet, body.idCasa],(err,res,fields)=>{
+                if(err) throw err;
+                resolve(res);
+            })
+        })
+    }
+
+    delete(id){
+        return new Promise((resolve,reject)=>{
+            db.query("DELETE FROM servicios WHERE id=?", [id],(err,res,fields)=>{
+                if(err) throw err;
+                resolve(res);
+            })
+        }) 
+    }
+
+}
+
+module.exports = ServicioModel;
