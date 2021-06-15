4e319796b66349c08a3bcde75f0e34a730121034
Franco Benitez <franco_casla_benitez@hotmail.com>
2021-06-15 15:03:18 -0300

    update

--- src/database/database.js
+++ src/database/database.js
@@ -4,21 +4,21 @@ const {config} = require('../../config/index');
 var db_config = {
   host: config.dbHost,
   user: config.dbUser,
-  password: config.dbPass,
   database: config.dbName,
+  password: config.dbPass,
   multipleStatements: true
 };
 
-const mysqlConnection = mysql.createConnection(db_config);
+const mysqlConnection = mysql.createPool(db_config);
 
 
-mysqlConnection.connect(function(err) {              
-  if(err) {                                     
-    console.log('error when connecting to db:', err); 
-    return;
-  }else {
-    console.log('DB id connected');
-  }                                     
-});                                     
+// mysqlConnection.connect(function(err) {              
+//   if(err) {                                     
+//     console.log('error when connecting to db:', err); 
+//     return;
+//   }else {
+//     console.log('DB id connected');
+//   }                                     
+// });                                     
 
 module.exports = mysqlConnection;
